1405a0d7cecf2f59977398b5c1ad59ea42088094
s13sx7 <s13sx7@gmail.com>
2024-10-15 21:25:01 +0700

    CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.categories.routers import router as router_categories
 from app.products.routers import router as router_products
 
@@ -6,3 +7,13 @@ from app.products.routers import router as router_products
 app = FastAPI()
 app.include_router(router_categories)
 app.include_router(router_products)
+
+origins = [
+    "http://localhost:8080"
+]
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=['GET', 'POST','PUT','PATCH','DELETE'],
+                   allow_headers=['*'])
